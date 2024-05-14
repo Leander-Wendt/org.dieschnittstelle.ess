020f422b6788156a5333796946ea7d295be18b8c
Leander Wendt <m1573ry@gmail.com>
2024-05-14 19:40:23 +0200

    Update ShowTouchpointRESTService.java

--- org.dieschnittstelle.ess.jrs.client/src/main/java/org/dieschnittstelle/ess/jrs/client/ShowTouchpointRESTService.java
+++ org.dieschnittstelle.ess.jrs.client/src/main/java/org/dieschnittstelle/ess/jrs/client/ShowTouchpointRESTService.java
@@ -82,6 +82,7 @@ public class ShowTouchpointRESTService {
 		/*
 		 * UE JRS1: add a call to the update method, passing tp
 		 */
+
 		logger.info("renamed touchpoint with id " + tp.getId() + " to " + tp.getName());
 
 		show("TestTouchpointRESTService: done.\n");
