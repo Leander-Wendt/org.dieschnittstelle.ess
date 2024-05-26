349dfe52dce9cffdf91ae3228b33a9c7dc518635
Leander Wendt <m1573ry@gmail.com>
2024-05-26 16:07:45 +0200

    Added additional annotations

--- org.dieschnittstelle.ess.mip.components.erp.api/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/api/ProductCRUD.java
+++ org.dieschnittstelle.ess.mip.components.erp.api/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/api/ProductCRUD.java
@@ -2,17 +2,21 @@ package org.dieschnittstelle.ess.mip.components.erp.crud.api;
 
 import java.util.List;
 
-import jakarta.persistence.PostLoad;
 import jakarta.ws.rs.*;
 import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
 import org.dieschnittstelle.ess.entities.erp.Campaign;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import jakarta.ws.rs.core.MediaType;
 
 /*
  * TODO MIP+JPA1/2/5:
  * this interface shall be implemented using an ApplicationScoped CDI bean with an EntityManager.
  * See TouchpointCRUDImpl for an example bean with a similar scope of functionality
  */
-// @Path("/products")
+@Path("/products")
+@RegisterRestClient
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public interface ProductCRUD {
 
 	@POST
