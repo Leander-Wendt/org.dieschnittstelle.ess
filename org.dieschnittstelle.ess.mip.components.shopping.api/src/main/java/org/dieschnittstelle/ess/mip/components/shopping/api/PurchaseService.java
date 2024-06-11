1ca31c9d80d29d9570a680932d41b7b4a587bf1a
Leander Wendt <s52888@bht-berlin.de>
2024-06-11 11:46:09 +0200

    Added annotations

--- org.dieschnittstelle.ess.mip.components.shopping.api/src/main/java/org/dieschnittstelle/ess/mip/components/shopping/api/PurchaseService.java
+++ org.dieschnittstelle.ess.mip.components.shopping.api/src/main/java/org/dieschnittstelle/ess/mip/components/shopping/api/PurchaseService.java
@@ -1,8 +1,15 @@
 package org.dieschnittstelle.ess.mip.components.shopping.api;
 
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
 // TODO: PAT1: this is the interface to be provided as a rest service if rest service access is used
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public interface PurchaseService {
 
-	void purchaseCartAtTouchpointForCustomer(long shoppingCartId, long touchpointId, long customerId) throws ShoppingException;
+	@POST
+	@Path("/{cartId}/purchase")
+	void purchaseCartAtTouchpointForCustomer(@PathParam("cartId") long shoppingCartId, @QueryParam("tpId") long touchpointId, @QueryParam("cusId") long customerId) throws ShoppingException;
 	
 }
