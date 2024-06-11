3e1bad081c49cf23aeeb99194d942958f9395723
Leander Wendt <s52888@bht-berlin.de>
2024-06-11 11:56:27 +0200

    Update PurchaseServiceClient.java

--- org.dieschnittstelle.ess.mip.client/src/main/java/org/dieschnittstelle/ess/mip/client/shopping/PurchaseServiceClient.java
+++ org.dieschnittstelle.ess.mip.client/src/main/java/org/dieschnittstelle/ess/mip/client/shopping/PurchaseServiceClient.java
@@ -1,6 +1,10 @@
 package org.dieschnittstelle.ess.mip.client.shopping;
 
-import org.apache.logging.log4j.Logger;
+import org.dieschnittstelle.ess.entities.erp.Campaign;
+import org.dieschnittstelle.ess.entities.shopping.ShoppingCartItem;
+import org.dieschnittstelle.ess.mip.client.apiclients.ServiceProxyFactory;
+import org.dieschnittstelle.ess.mip.client.apiclients.ShoppingCartClient;
+import org.dieschnittstelle.ess.mip.components.shopping.api.PurchaseService;
 import org.dieschnittstelle.ess.mip.components.shopping.api.ShoppingException;
 import org.dieschnittstelle.ess.entities.crm.AbstractTouchpoint;
 import org.dieschnittstelle.ess.entities.crm.Customer;
@@ -8,40 +12,46 @@ import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
 
 public class PurchaseServiceClient implements ShoppingBusinessDelegate {
 
-	protected static Logger logger = org.apache.logging.log4j.LogManager
-			.getLogger(PurchaseServiceClient.class);
+    private AbstractTouchpoint abstractTouchpoint;
+    private Customer customer;
+    private ShoppingCartClient shoppingCartClient;
+    private PurchaseService purchaseServiceProxy;
+    /*
+     * TODO PAT1: use an appropriate proxy for the server-side purchase service
+     *  Note that touchpoint and customer need to be stored locally
+     *  before purchase() is invoked. For accessing shopping cart data use a local ShoppingCartClient
+     *  in this case and access the shopping cart using the provided getter method
+     */
 
-	/*
-	 * TODO PAT1: use an appropriate proxy for the server-side purchase service
-	 *  Note that touchpoint and customer need to be stored locally
-	 *  before purchase() is invoked. For accessing shopping cart data use a local ShoppingCartClient
-	 *  in this case and access the shopping cart using the provided getter method
-	 */
+    public PurchaseServiceClient() {
+        purchaseServiceProxy = ServiceProxyFactory.getInstance().getProxy(PurchaseService.class);
+        try {
+            this.shoppingCartClient = new ShoppingCartClient();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public PurchaseServiceClient() {
-		/* TODO: instantiate the proxy using the ServiceProxyFactory (see the other client classes) */
-	}
+    /* TODO: implement the following methods s */
 
-	/* TODO: implement the following methods s */
+    @Override
+    public void setTouchpoint(AbstractTouchpoint touchpoint) {
+        this.abstractTouchpoint = touchpoint;
+    }
 
-	@Override
-	public void setTouchpoint(AbstractTouchpoint touchpoint) {
-	
-	}
+    @Override
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 
-	@Override
-	public void setCustomer(Customer customer) {
-	
-	}
+    @Override
+    public void addProduct(AbstractProduct product, int units) {
+        this.shoppingCartClient.addItem(new ShoppingCartItem(product.getId(), units, product instanceof Campaign));
+    }
 
-	@Override
-	public void addProduct(AbstractProduct product, int units) {
-	
-	}
-
-	@Override
-	public void purchase() throws ShoppingException {
-	
-	}
+    @Override
+    public void purchase() throws ShoppingException {
+        this.purchaseServiceProxy.purchaseCartAtTouchpointForCustomer(this.shoppingCartClient.getShoppingCartEntityId(), abstractTouchpoint.getId(), customer.getId());
+    }
 
 }
