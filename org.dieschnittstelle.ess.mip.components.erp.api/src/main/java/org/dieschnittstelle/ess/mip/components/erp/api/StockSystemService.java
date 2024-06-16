e253dd2cd78ba584c88b700abb518615f0079f55
Leander Wendt <m1573ry@gmail.com>
2024-06-16 18:32:49 +0200

    Added @RegisterRestClient annotation

--- org.dieschnittstelle.ess.mip.components.erp.api/src/main/java/org/dieschnittstelle/ess/mip/components/erp/api/StockSystemService.java
+++ org.dieschnittstelle.ess.mip.components.erp.api/src/main/java/org/dieschnittstelle/ess/mip/components/erp/api/StockSystemService.java
@@ -3,6 +3,7 @@ package org.dieschnittstelle.ess.mip.components.erp.api;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.dieschnittstelle.ess.entities.erp.IndividualisedProductItem;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import java.awt.*;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.List;
  * - let the StockSystemClient in the client project access the web api via this interface - see ShoppingCartClient for an example
  */
 @Path("/stock")
+@RegisterRestClient
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public interface StockSystemService {
