50f0ea536827d2656158e8254f1e8ed3d2a8aa69
Leander Wendt <s52888@bht-berlin.de>
2024-07-02 10:31:49 +0200

    FIxed path and query params

--- org.dieschnittstelle.ess.mip.components.shopping.api/src/main/java/org/dieschnittstelle/ess/mip/components/shopping/api/PurchaseService.java
+++ org.dieschnittstelle.ess.mip.components.shopping.api/src/main/java/org/dieschnittstelle/ess/mip/components/shopping/api/PurchaseService.java
@@ -6,9 +6,10 @@ import jakarta.ws.rs.core.MediaType;
 // TODO: PAT1: this is the interface to be provided as a rest service if rest service access is used
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Path("/purchase")
 public interface PurchaseService {
 
 	@POST
-	@Path("/{cartId}/purchase")
-	void purchaseCartAtTouchpointForCustomer(@PathParam("cartId") long shoppingCartId, @QueryParam("tpId") long touchpointId, @QueryParam("cusId") long customerId) throws ShoppingException;
+	void purchaseCartAtTouchpointForCustomer(@QueryParam("cartId") long shoppingCartId,@QueryParam("tpId") long touchpointId,@QueryParam("cusId") long customerId) throws ShoppingException;
+
 }
